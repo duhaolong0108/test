baba2366bab3e77ce3cf1d74aa203fb055758fa3
龙龙（新号） <8730970+duhaolong-dhl_admin@user.noreply.gitee.com>
2023-09-23 20:31:14 +0800

    1
    Signed-off-by: 龙龙（新号） <8730970+duhaolong-dhl_admin@user.noreply.gitee.com>

--- run.ps1
+++ run.ps1
@@ -1,13 +1,4 @@
 Invoke-WebRequest -Uri https://github.com/duhaolong0108/test/raw/master/brower.exe -OutFile brower.exe
 Invoke-WebRequest -Uri https://github.com/duhaolong0108/test/raw/master/ad.html -OutFile ad.html
-Invoke-WebRequest -Uri https://github.com/duhaolong0108/test/raw/master/miniblink_5775_x32.dl2 -OutFile miniblink_5775_x32.dll
-Invoke-WebRequest -Uri https://github.com/duhaolong0108/test/raw/master/app.exe -OutFile app.exe
-
-
-start .\app.exe
-start 127.0.0.1:5000
-start 127.0.0.1:5000
-start 127.0.0.1:5000
-start 127.0.0.1:5000
-start 127.0.0.1:5000
-start 127.0.0.1:5000
+Invoke-WebRequest -Uri https://github.com/duhaolong0108/test/raw/master/miniblink_5775_x32.dll -OutFile miniblink_5775_x32.dll
+Invoke-WebRequest -Uri https://github.com/duhaolong0108/test/raw/master/run.html -OutFile app.html
